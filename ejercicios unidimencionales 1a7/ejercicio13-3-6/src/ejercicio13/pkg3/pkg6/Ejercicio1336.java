26a46234c6c1abaee4b70b910936e7444e9263ae
Santiago172gh <132229738+Santiago172gh@users.noreply.github.com>
2023-05-26 17:26:11 -0500

    Actualizar Ejercicio1336.java

--- ejercicios unidimencionales 1a7/ejercicio13-3-6/src/ejercicio13/pkg3/pkg6/Ejercicio1336.java
+++ ejercicios unidimencionales 1a7/ejercicio13-3-6/src/ejercicio13/pkg3/pkg6/Ejercicio1336.java
@@ -30,3 +30,48 @@ public class Ejercicio1336 {
     }
 }
 
+
+import java.util.Scanner;
+
+public class Ejercicio1336 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int contar;
+        
+        System.out.println("Ingrese hasta qué número desea contar: ");
+        contar = scan.nextInt();
+        
+        int[] num = new int[contar];
+        
+        for (int i = 0; i < contar; i++) {
+            num[i] = i + 1;
+        }
+        
+        System.out.println("Números primos:");
+        
+        for (int i = 0; i < contar; i++) {
+            if (esPrimo(num[i])) {
+                System.out.println("Número primo: " + num[i]);
+            }
+        }
+    }
+    
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+
+
