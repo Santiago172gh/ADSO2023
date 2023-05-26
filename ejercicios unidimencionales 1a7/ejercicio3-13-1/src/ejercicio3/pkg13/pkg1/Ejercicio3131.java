2c190818a7fd14cb4ccfd91709c310636fd863d6
Santiago172gh <132229738+Santiago172gh@users.noreply.github.com>
2023-05-26 13:13:52 -0500

    Update Ejercicio3131.java

--- ejercicios unidimencionales 1a7/ejercicio3-13-1/src/ejercicio3/pkg13/pkg1/Ejercicio3131.java
+++ ejercicios unidimencionales 1a7/ejercicio3-13-1/src/ejercicio3/pkg13/pkg1/Ejercicio3131.java
@@ -1,12 +1,9 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio3.pkg13.pkg1;
 
 /**
  *
- * @author Maira Alejandra
  */
 public class Ejercicio3131 {
 
